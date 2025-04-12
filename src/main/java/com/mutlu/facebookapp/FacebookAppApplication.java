afc9e6aee689c15441cf4655b2e9e1867449bc8b
7sleepless24 <mutluonur1998@gmail.com>
2025-04-12 17:59:02 +0300

    onur

---
+++ src/main/java/com/mutlu/facebookapp/FacebookAppApplication.java
@@ -0,0 +1,13 @@
+package com.mutlu.facebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookAppApplication.class, args);
+    }
+
+}
